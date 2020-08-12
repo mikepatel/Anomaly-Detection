76872efb7e794c2c1646d4f742fe5566e0ff702c
Michael Patel <michael.patel94@gmail.com>
2020-08-11 23:32:49 -0400

    Setting up conda env

---
+++ train.py
@@ -0,0 +1,15 @@
+"""
+
+
+conda activate anomaly
+"""
+################################################################################
+# Imports
+from parameters import *
+
+
+################################################################################
+# Main
+if __name__ == "__main__":
+    # print TF version
+    print(f'TF version: {tf.__version__}')
