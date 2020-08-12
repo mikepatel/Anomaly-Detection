51899763b73b6ecf989bdafe2436eaee1abd6a61
Michael Patel <michael.patel94@gmail.com>
2020-08-12 00:09:50 -0400

    Using fashion mnist

--- train.py
+++ train.py
@@ -13,3 +13,31 @@ from parameters import *
 if __name__ == "__main__":
     # print TF version
     print(f'TF version: {tf.__version__}')
+
+    # ----- ETL ----- #
+    # ETL = Extraction, Transformation, Load
+    (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
+
+    print(f'Shape of train images: {train_images.shape}')
+    print(f'Shape of train labels: {train_labels.shape}')
+    print(f'Shape of test images: {test_images.shape}')
+    print(f'Shape of test labels: {test_labels.shape}')
+
+    # class labels
+    categories = [
+        "t-shirt",
+        "trouser",
+        "pullover",
+        "dress",
+        "coat",
+        "sandal",
+        "shirt",
+        "sneaker",
+        "bag",
+        "ankle boot"
+    ]
+
+    class2int = {u: i for i, u in enumerate(categories)}
+    int2class = {i: u for i, u in enumerate(categories)}
+
+    print(class2int)
