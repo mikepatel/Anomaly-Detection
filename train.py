cc1c31917176c1e4c59eb1a85638c2c9eccc5f9a
Michael Patel <michael.patel94@gmail.com>
2020-08-12 00:40:59 -0400

    Adding model file

--- train.py
+++ train.py
@@ -1,7 +1,12 @@
 """
+Michael Patel
+August 2020
 
+Project description:
 
-conda activate anomaly
+File description:
+
+conda activate Anomaly-Detection
 """
 ################################################################################
 # Imports
